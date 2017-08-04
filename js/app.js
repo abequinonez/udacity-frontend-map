f14cb53a1aaf8e193b5b74a2d3bc2f924898e46d
Abraham Quinonez <abequinonez@users.noreply.github.com>
2017-08-03 18:38:51 -0700

    Changed Google marker icon link

--- js/app.js
+++ js/app.js
@@ -111,7 +111,7 @@ function bounce(marker) {
 // Creates a marker icon. Called by createMarkers().
 function createMarkerIcon(color) {
 	var markerImage = new google.maps.MarkerImage(
-		'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + color +
+		'https://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + color +
 		'|40|_|%E2%80%A2',
 		new google.maps.Size(21, 34),
 		new google.maps.Point(0, 0),
